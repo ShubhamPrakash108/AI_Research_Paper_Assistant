4e66c6120b267387bfe9ffd5e0296a3c6cf2fcb2
Shubham Prakash <157580501+ShubhamPrakash108@users.noreply.github.com>
2025-02-17 20:43:53 +0530

    Update A4_ques_and_ans.py

--- A4_ques_and_ans.py
+++ A4_ques_and_ans.py
@@ -10,19 +10,7 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain.chains import create_retrieval_chain
 import random
 
-GEMINI_API_KEYS = [
-    "AIzaSyCzTWJ5ZpkTGcnfabAXQjOSvMqrcwG7onk",
-    "AIzaSyDqJBsNCbJEHp80nXTS4BzNOMD7e3KSQ10",
-    "AIzaSyBPgJh_sRwKLAz0jR2EpAIy-F2zmKPR2qQ",
-    "AIzaSyAYr05BjrCqRjNTrAvv2a50yPoe1Cpgw4A",
-    "AIzaSyCchpkS1qxo2fqwT15H7BGd-41Tn-n9M24",
-    "AIzaSyDfokL9HczAzRasWl_GNtT41E0gA2jkUJU",
-    "AIzaSyD13qlRA23cAWjHPkb1uBaNdCUpBuPxoI0",
-    "AIzaSyCRtKweOh3JPH8Gn1ACMrejuiQIW0N3RsY",
-    "AIzaSyBlUyW5_eKud705mqzfo_athWNF-UgGlJY",
-    "AIzaSyDxM-jtsl6DF4lm67lFWIumRXQACeZcHqo",
-    "AIzaSyAteEW-4HLXkAudnDSKudFEyVP6rN9FT2c"
-]
+GEMINI_API_KEYS = ["","","","","","","","","","",""]
 
 
 
